7392924ce5984232838f53f68b5e52eb29274081
Maximiliano Sandoval <maxisandoval98@gmail.com>
2022-04-13 21:02:16 -0300

    created first model and view: tiposCuentas

---
+++ Controllers/TiposCuentasController.cs
@@ -0,0 +1,19 @@
+﻿using gestorPresupuestos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gestorPresupuestos.Controllers
+{
+    public class TiposCuentasController: Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/TipoCuenta.cs
@@ -0,0 +1,11 @@
+﻿namespace gestorPresupuestos.Models
+{
+    public class TipoCuenta
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int usuarioId { get; set; }
+        public int Orden { get; set; }
+
+    }
+}
