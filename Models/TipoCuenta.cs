2c5c11f08f5126952589b7d9541a4bf8c3589f7d
Maximiliano Sandoval <maxisandoval98@gmail.com>
2022-04-15 11:25:16 -0300

    added validations front

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -9,6 +9,7 @@ namespace gestorPresupuestos.Models
         [Required(ErrorMessage = "El campo nombre es obligatorio")]
         [StringLength(maximumLength:50,MinimumLength = 3, 
         ErrorMessage = "El campo {0} debe estar entre {2} y {1}")]
+        [Display(Name = "Nombre del tipo de cuenta: ")]
         public string nombre { get; set; }
         public int usuarioId { get; set; }
         public int orden { get; set; }
