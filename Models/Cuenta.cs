8f920db7f33b37f014b09e472c839587c95943bf
Maximiliano Sandoval <maxisandoval98@gmail.com>
2022-05-23 19:39:43 -0300

    updates display names for accounts and minor fix in home index

--- Models/Cuenta.cs
+++ Models/Cuenta.cs
@@ -7,11 +7,17 @@ namespace gestorPresupuestos.Models
         public int id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength: 50)]
+        [Display(Name = "Nombre")]
         public string nombre { get; set; }
+        
         [Display(Name = "Tipo de Cuenta")]
         public int tipoCuentaId { get; set; }
+        
+        [Display(Name = "Balance")]
         public decimal balance { get; set; }
+        
         [StringLength(maximumLength: 1000)]
+        [Display(Name = "Descripción")]
         public string descripcion { get; set; }
 
     }
