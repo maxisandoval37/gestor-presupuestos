a56d9aebd2e265e87403704d5e15aaac962b7418
maxisandoval37 <maxisandoval98@gmail.com>
2022-06-21 21:44:59 -0300

    added transaccion model and repository

---
+++ Models/Transaccion.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gestorPresupuestos.Models
+{
+    public class Transaccion
+    {
+        public int id { get; set; }
+        public int usuarioId { get; set; }
+        public DateTime fechaTransaccion { get; set; } = DateTime.Today;
+        public decimal monto { get; set; }
+        [Range(0, maximum: int.MaxValue, ErrorMessage = "La categoría es obligatoria")]
+        public int categoriaId { get; set; }
+        public string nota { get; set; }
+        [Range(0, maximum: int.MaxValue, ErrorMessage = "La cuenta es obligatoria")]
+        public int cuentaId { get; set; }
+    }
+}
---
+++ Servicios/TransaccionRepository.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using gestorPresupuestos.Models;
+using Microsoft.Data.SqlClient;
+
+namespace gestorPresupuestos.Servicios
+{
+    public interface ITransaccionRepository
+    {
+        Task Insertar(Transaccion transaccion);
+
+    }
+    public class TransaccionRepository : ITransaccionRepository
+    {
+        private readonly string connectionString;
+        public TransaccionRepository(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Insertar(Transaccion transaccion)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            var id = await connection.QuerySingleAsync<int>("TIPOCUENTA_INSERTAR",
+                new
+                {
+                    usuario_id = transaccion.usuarioId,
+                    fecha_transaccion = transaccion.fechaTransaccion,
+                    monto = transaccion.monto,
+                    categoria_id = transaccion.categoriaId,
+                    cuenta_id = transaccion.cuentaId,
+                    nota = transaccion.nota
+                },
+                commandType: System.Data.CommandType.StoredProcedure);
+
+            transaccion.id = id;
+            //TODO Crear SP en la DB
+        }
+    }
+}
