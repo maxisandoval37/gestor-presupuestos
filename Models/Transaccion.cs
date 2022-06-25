2caddacee8d56abad5de330d06edad323c9a354e
maxisandoval37 <maxisandoval98@gmail.com>
2022-06-25 18:20:33 -0300

    added transaction insert view and minor changes

--- Models/Transaccion.cs
+++ Models/Transaccion.cs
@@ -6,12 +6,16 @@ namespace gestorPresupuestos.Models
     {
         public int id { get; set; }
         public int usuarioId { get; set; }
-        public DateTime fechaTransaccion { get; set; } = DateTime.Today;
+        public DateTime fechaTransaccion { get; set; } = DateTime.Parse(DateTime.Now.ToString("dd-MM-yyyy hh:mm tt"));
         public decimal monto { get; set; }
+        
         [Range(0, maximum: int.MaxValue, ErrorMessage = "La categoría es obligatoria")]
+        [Display(Name = "Categoría")]
         public int categoriaId { get; set; }
         public string nota { get; set; }
         [Range(0, maximum: int.MaxValue, ErrorMessage = "La cuenta es obligatoria")]
+        
+        [Display(Name = "Cuenta")]
         public int cuentaId { get; set; }
     }
 }
--- Models/TransaccionCreacionViewModel.cs
+++ Models/TransaccionCreacionViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace gestorPresupuestos.Models
 {
     public class TransaccionCreacionViewModel: Transaccion
     {
         public IEnumerable<SelectListItem> Cuentas { get; set; }
-
         public IEnumerable<SelectListItem> Categorias { get; set; }
+        
+        [Display(Name = "Tipo de Operación")]
+        public TipoOperacion tipoOperacionId { get; set; }
     }
 }
