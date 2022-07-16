a7b0a7ade55a47a5ea921ecff96ee7eaf7c46833
maxisandoval37 <maxisandoval98@gmail.com>
2022-07-16 19:20:23 -0300

    extract transaction method js

---
+++ wwwroot/js/formTransaccion.js
@@ -0,0 +1,16 @@
+﻿async function llenarListaCategorias(urlCategorias, valorSeleccionado) {
+    //hacer peticion para obtener las categorias:
+    const respuesta = await fetch(urlCategorias, {
+        method: 'POST',
+        body: valorSeleccionado,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const json = await respuesta.json();
+
+    //llenar lista desplegable de categorias:
+    const opciones = json.map(categoria => `<option value=${categoria.value}>${categoria.text}</option>`);
+    $("#categoriaId").html(opciones);
+}
