56ff7f3a2877bfd60cef8be0bd528c623b3f8f8c
Maxi Sandoval <maxisandoval98@gmail.com>
2022-10-16 16:26:23 -0300

    added weekly report and decoupled amounts view

---
+++ Models/Submenus/ResumenMontosViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace gestorPresupuestos.Models.Submenus
+{
+    public class ResumenMontosViewModel
+    {
+        public decimal ingresos { get; set; }
+        public decimal egresos { get; set; }
+        public decimal total { get; set; }
+    }
+}
